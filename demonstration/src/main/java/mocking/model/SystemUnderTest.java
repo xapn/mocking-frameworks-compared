70534d249eda2e5704a8643b72488d934e282943
Xavier Pigeon <xpn.projects@gmail.com>
2015-08-26 17:02:10 +0200

    [SUT] Design the class model for mocking demonstration. #2
    - Create a simple model.

---
+++ demonstration/src/main/java/mocking/model/SystemUnderTest.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package mocking.model;
+
+/**
+ * The object class to be tested.
+ * 
+ * @author Xavier Pigeon
+ */
+public class SystemUnderTest {
+
+    private HowItBehaves howItBehaves;
+
+    public SystemUnderTest() {
+    }
+
+    public SystemUnderTest(HowItBehaves howItBehaves) {
+        this.howItBehaves = howItBehaves;
+    }
+
+    /**
+     * The method to be tested.
+     */
+    public void process() {
+        howItBehaves.whereItCallsDependencies();
+    }
+}
