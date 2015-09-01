55b254483cf6e5ecb920cdd8600bf095b35c136a
Xavier Pigeon <xpn.projects@gmail.com>
2015-09-01 13:01:52 +0200

    [EasyMock] Stub void methods. #4
    - The mock expects that its called void method throws an exception.

--- demonstration/src/test/java/mocking/voidmethod/VoidMethodWithEasyMockTest.java
+++ demonstration/src/test/java/mocking/voidmethod/VoidMethodWithEasyMockTest.java
@@ -4,6 +4,7 @@
 package mocking.voidmethod;
 
 import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expectLastCall;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 import mocking.model.HowItBehaves;
@@ -12,6 +13,8 @@ import mocking.model.ToBeMocked;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This test case shows how to stub a void method with EasyMock.
@@ -19,6 +22,8 @@ import org.junit.Test;
  * @author Xavier Pigeon
  */
 public class VoidMethodWithEasyMockTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VoidMethodWithEasyMockTest.class);
 
     private SystemUnderTest systemUnderTest;
 
@@ -44,6 +49,32 @@ public class VoidMethodWithEasyMockTest {
         });
         // Mock expectations
         mock.voidMethod("expected call");
+        replay(mock);
+
+        // WHEN
+        systemUnderTest.process();
+
+        // THEN
+        verify(mock);
+    }
+
+    @Test
+    public void should_call_an_expected_void_method_throwing_exception() throws Exception {
+        // GIVEN
+        systemUnderTest = new SystemUnderTest(new HowItBehaves() {
+
+            @Override
+            public void whereItCallsDependencies() {
+                try {
+                    mock.voidMethodThrowingException("expected call");
+                } catch (Exception e) {
+                    LOGGER.debug(e.getMessage());
+                }
+            }
+        });
+        // Mock expectations
+        mock.voidMethodThrowingException("expected call");
+        expectLastCall().andThrow(new Exception("This void method throws an exception!"));
         replay(mock);
 
         // WHEN
