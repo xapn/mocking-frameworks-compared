e22108156331ed2f77219033765e4a6e0a4c66ca
Xavier Pigeon <xpn.projects@gmail.com>
2015-09-01 13:59:10 +0200

    [Mockito] Stub void methods. #4
    - The mock expects that its called void method throws an exception.

--- demonstration/src/test/java/mocking/voidmethod/VoidMethodWithMockitoTest.java
+++ demonstration/src/test/java/mocking/voidmethod/VoidMethodWithMockitoTest.java
@@ -4,6 +4,7 @@
 package mocking.voidmethod;
 
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import mocking.model.HowItBehaves;
 import mocking.model.SystemUnderTest;
@@ -12,6 +13,8 @@ import mocking.model.ToBeMocked;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * This test case shows how to stub a void method with Mockito.
@@ -19,6 +22,8 @@ import org.mockito.Mockito;
  * @author Xavier Pigeon
  */
 public class VoidMethodWithMockitoTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(VoidMethodWithMockitoTest.class);
 
     private SystemUnderTest systemUnderTest;
 
@@ -50,5 +55,29 @@ public class VoidMethodWithMockitoTest {
 
         // THEN
         verify(mock).voidMethod("expected call");
+    }
+
+    @Test
+    public void should_call_an_expected_void_method_throwing_exception() throws Exception {
+        // GIVEN
+        systemUnderTest = new SystemUnderTest(new HowItBehaves() {
+
+            @Override
+            public void whereItCallsDependencies() {
+                try {
+                    mock.voidMethodThrowingException("expected call");
+                } catch (Exception e) {
+                    LOGGER.debug(e.getMessage());
+                }
+            }
+        });
+        // Mock expectations
+        doThrow(Exception.class).when(mock).voidMethodThrowingException("expected call");
+
+        // WHEN
+        systemUnderTest.process();
+
+        // THEN
+        verify(mock).voidMethodThrowingException("expected call");
     }
 }
